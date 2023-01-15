de7b07b4d28b3041a723dda1771919eee300e652
Ben Banister <desidiawvu@gmail.com>
2023-01-15 15:42:38 -0500

    tf fmt

--- terraform/discord-lambda/main.tf
+++ terraform/discord-lambda/main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "build" {
   }
   provisioner "local-exec" {
     working_dir = "${path.module}/go"
-    command = "go build"
+    command     = "go build"
   }
 }
 
@@ -44,18 +44,18 @@ EOF
 resource "aws_lambda_function" "valheim_discord" {
   function_name    = "valheim_discord"
   filename         = "${path.module}/${local.script_name}.zip"
-  handler          = "${local.script_name}"
-  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = local.script_name
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role             = aws_iam_role.iam_for_lambda.arn
   runtime          = "go1.x"
   memory_size      = 128
   timeout          = 10
   environment {
     variables = {
-      DISCORD_KEY = "${var.discord_public_key}"
-      SERVICE_ARN = "${var.ecs_service_arn}"
+      DISCORD_KEY  = "${var.discord_public_key}"
+      SERVICE_ARN  = "${var.ecs_service_arn}"
       CLUSTER_NAME = "${var.ecs_cluster_name}"
-      REGION = "${var.aws_region}"
+      REGION       = "${var.aws_region}"
     }
   }
 }
@@ -66,25 +66,25 @@ resource "aws_api_gateway_rest_api" "valheim_discord_api" {
 
 
 resource "aws_api_gateway_method" "valheim_gateway_method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.valheim_discord_api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.valheim_discord_api.root_resource_id}"
+  rest_api_id   = aws_api_gateway_rest_api.valheim_discord_api.id
+  resource_id   = aws_api_gateway_rest_api.valheim_discord_api.root_resource_id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.valheim_discord_api.id}"
-  resource_id             = "${aws_api_gateway_rest_api.valheim_discord_api.root_resource_id}"
-  http_method             = "${aws_api_gateway_method.valheim_gateway_method.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.valheim_discord_api.id
+  resource_id             = aws_api_gateway_rest_api.valheim_discord_api.root_resource_id
+  http_method             = aws_api_gateway_method.valheim_gateway_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.valheim_discord.invoke_arn}"
+  uri                     = aws_lambda_function.valheim_discord.invoke_arn
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.valheim_discord.function_name}"
+  function_name = aws_lambda_function.valheim_discord.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.valheim_discord_api.execution_arn}/*/*/*"
@@ -93,13 +93,13 @@ resource "aws_lambda_permission" "apigw_lambda" {
 resource "aws_api_gateway_deployment" "valheim_gateway_deployment" {
   depends_on = [aws_api_gateway_integration.integration]
 
-  rest_api_id = "${aws_api_gateway_rest_api.valheim_discord_api.id}"
+  rest_api_id = aws_api_gateway_rest_api.valheim_discord_api.id
   stage_name  = "v1"
 
 }
 
 output "url" {
-  value = "${aws_api_gateway_deployment.valheim_gateway_deployment.invoke_url}"
+  value = aws_api_gateway_deployment.valheim_gateway_deployment.invoke_url
 }
 
 
@@ -112,7 +112,7 @@ resource "aws_cloudwatch_log_group" "function_log_group" {
 }
 
 resource "aws_iam_policy" "function_policy" {
-  name   = "function-policy"
+  name = "function-policy"
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -137,6 +137,6 @@ resource "aws_iam_policy" "function_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "function_policy_attachment" {
-  role = aws_iam_role.iam_for_lambda.id
+  role       = aws_iam_role.iam_for_lambda.id
   policy_arn = aws_iam_policy.function_policy.arn
 }
